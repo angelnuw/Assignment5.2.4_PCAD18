f8c642bc63e16df98142d218a1761959a55e3d79
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-08-05 17:29:32 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Assignment5._2._4
+{
+    internal class Program
+    {
+        static void Palindrome_Recursion(string str, int start, int end)
+        {
+            bool isPalindrome = true;
+            bool notPalindrome = false;
+
+            if (start >= end)
+            {
+                Console.WriteLine($"{isPalindrome}");
+                return;
+            }
+            if (str[start] != str[end])
+            {
+                Console.WriteLine($"{notPalindrome}");
+                return;
+            }
+            Palindrome_Recursion(str, start + 1, end - 1);
+        }
+        static void Palindrome(string str)
+        {
+            str = str.ToLower().Replace(" ", "");
+            Palindrome_Recursion(str, 0, str.Length - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string to check if it is a palindrome:");
+            string input = Console.ReadLine();
+            Palindrome(input);
+        }
+    }
+}
